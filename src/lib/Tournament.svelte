0c4b681836377c0f7f74c11ba242db33ece4760c
emrergin <emrergin2757@yahoo.com>
2022-05-24 01:56:49 +0300

    improved graph

--- src/lib/Tournament.svelte
+++ src/lib/Tournament.svelte
@@ -12,11 +12,6 @@
     function draw(candidates) {
         if (document.getElementById(`canvas`)){
             let angle=(2*Math.PI)/candidates.length;
-            // let maximumCharacter = candidates.reduce(
-            //     function (a, b) {
-            //         return a.length > b.length ? a : b;
-            //     }
-            // ).length;
 
             let candidateLocations=[];
             for (let i = 0; i < candidates.length; i++) {
@@ -28,16 +23,9 @@
             }        
             var ctx = document.getElementById('canvas').getContext('2d');
             ctx.clearRect(0, 0, canvas.width, canvas.height);
-            //Write alternative names.
-            ctx.font = `40px sans-serif`;  
-            ctx.fillStyle = "#2563eb";
-            ctx.textBaseline = 'middle';
-            ctx.textAlign="center";     
-            for (let i = 0; i < candidates.length; i++) {
-                ctx.fillText(candidateLocations[i].name, candidateLocations[i].loc_x, candidateLocations[i].loc_y,50);
-            }
+
             
-            ctx.beginPath();
+            
             ctx.strokeStyle=`#ea580c`;
             ctx.lineWidth = 2;
             ctx.font = '24px sans-serif'; 
@@ -49,13 +37,11 @@
                 let bitisAlternative = candidateLocations.filter(a=>a.name===bitisNoktasi)[0];
                 let x1=baslangicAlternative.loc_x;
                 let y1=baslangicAlternative.loc_y;
-                let x2=bitisAlternative.loc_x;
-                let y2=bitisAlternative.loc_y;
-                let rot = Math.atan2(y2-y1,x2-x1);
+                let x2=(bitisAlternative.loc_x*12+x1)/13;
+                let y2=(bitisAlternative.loc_y*12+y1)/13;
+                let rot = Math.atan2(y2-y1,x2-x1);                
 
-                //Write tournament margins.
-                ctx.fillText(tournamentData[data],(x1+2*x2)/3, (y1+2*y2)/3);
-
+                ctx.beginPath();
                 ctx.moveTo(x1, y1);
                 ctx.lineTo(x2, y2);
                 ctx.stroke();
@@ -67,10 +53,25 @@
                 //path from the side point back to the tip of the arrow, and then again to the opposite side point
                 ctx.lineTo(x2, y2);
                 ctx.lineTo(x2-headlen*Math.cos(rot-Math.PI/7),y2-headlen*Math.sin(rot-Math.PI/7));
+                ctx.closePath();
                 ctx.stroke();
-                ctx.fillStyle = "#2563eb";
+                ctx.fillStyle = "#93c5fd";
                 ctx.fill();
-            }    
+
+                //Write tournament margins.
+                ctx.fillStyle = "#2563eb";
+                ctx.fillText(tournamentData[data],(3*x1+2*x2)/5, (2*y1+3*y2)/5);
+
+            }   
+            
+            //Write alternative names.
+            ctx.font = `40px sans-serif`;  
+            ctx.fillStyle = "#2563eb";
+            ctx.textBaseline = 'middle';
+            ctx.textAlign="center";     
+            for (let i = 0; i < candidates.length; i++) {
+                ctx.fillText(candidateLocations[i].name, candidateLocations[i].loc_x, candidateLocations[i].loc_y,50);
+            }
         }
                   
     }
@@ -84,5 +85,3 @@
 
 <canvas  width="400" height="400" class="bg-orange-50 mb-4 border-2 border-blue-400 rounded" id="canvas">
 </canvas>
-
-<!-- <p>{candidates}</p> -->
